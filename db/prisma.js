69e388ac9dd1354287519c0dce17877845403153
Goran Jovic <goran1010jovic@gmail.com>
2025-09-20 09:25:48 +0200

    chore: remove prod in NODE_ENV as not needed

--- db/prisma.js
+++ db/prisma.js
@@ -1,17 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
-let databaseUrl;
-
-switch (process.env.NODE_ENV) {
-  case "test":
-    databaseUrl = process.env.TEST_DATABASE_URL;
-    break;
-  case "production":
-    databaseUrl = process.env.PROD_DATABASE_URL;
-    break;
-  default:
-    databaseUrl = process.env.DATABASE_URL;
-}
+const databaseUrl =
+  process.env.NODE_ENV === "test"
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL;
 
 const prisma = new PrismaClient({
   datasources: {
