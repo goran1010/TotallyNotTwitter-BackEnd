2b806d7e9261a3505ab4bf19617ac9cde66846c8
Goran Jovic <goran1010jovic@gmail.com>
2025-10-24 11:50:08 +0200

    feat: add logout route and controller

--- controllers/statusController.js
+++ controllers/statusController.js
@@ -2,3 +2,16 @@ export function status(req, res) {
   const user = req.user;
   res.status(200).json({ id: user.id, username: user.username });
 }
+
+export function logout(req, res, next) {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.json({ message: "Successfully logged out." });
+  });
+}
+
+export function updateProfile() {
+  //code here
+}
--- routes/statusRouter.js
+++ routes/statusRouter.js
@@ -4,4 +4,8 @@ import * as statusController from "../controllers/statusController.js";
 
 statusRouter.get("/", statusController.status);
 
+statusRouter.get("/update-profile", statusController.updateProfile);
+
+statusRouter.get("/logout", statusController.logout);
+
 export default statusRouter;
