b97dcfb54d421312669d2d8f63b8b72a07e7bdd8
Goran Jovic <goran1010jovic@gmail.com>
2025-10-24 10:53:54 +0200

    feat: create profile and connect to user when creating new user

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -7,8 +7,11 @@ export async function signUp(req, res) {
     const { username, email, password } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    await prisma.user.create({
+    const user = await prisma.user.create({
       data: { username, email, password: hashedPassword },
+    });
+    await prisma.profile.create({
+      data: { user: { connect: { id: user.id } } },
     });
     return res.status(201).json({ message: "User created successfully" });
   } catch (err) {
