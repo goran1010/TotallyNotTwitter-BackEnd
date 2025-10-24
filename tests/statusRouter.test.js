cb21926af83edd42054ec06ed69990ca55ee3b2d
Goran Jovic <goran1010jovic@gmail.com>
2025-10-24 18:38:23 +0200

    test: add test for successful logout

--- tests/statusRouter.test.js
+++ tests/statusRouter.test.js
@@ -34,4 +34,30 @@ describe("GET /status route", () => {
     expect(response.body).toHaveProperty("id");
     expect(response.body).toHaveProperty("username", "test_user");
   });
+
+  test("responds with json 200 when logged out successfully", async () => {
+    const agent = request.agent(app);
+
+    await agent.post("/signup").send({
+      username: "test_user",
+      email: "testuser@example.com",
+      password: "secure_password123",
+      ["confirm-password"]: "secure_password123",
+    });
+
+    await agent
+      .post("/login")
+      .send({ username: "test_user", password: "secure_password123" })
+      .expect(200);
+
+    const response = await agent.get("/status");
+    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.status).toEqual(200);
+
+    await agent.post("/status/logout").expect(200);
+
+    const responseLogOut = await agent.get("/status");
+    expect(responseLogOut.headers["content-type"]).toMatch(/json/);
+    expect(responseLogOut.status).toEqual(403);
+  });
 });
